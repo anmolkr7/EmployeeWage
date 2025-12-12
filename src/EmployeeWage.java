9dac5c355cac91f3f36d7ebbbcfc71f63cb0afe4
anmolkr7 <anmolojha952@gmail.com>
2025-12-12 13:43:45 +0530

    UC8:Compute wages for multiple companies using parameters

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,23 +1,16 @@
-
 public class EmployeeWage {
-    // Constants
-    private static final int IS_FULL_TIME = 1;
-    private static final int IS_PART_TIME = 2;
-    private static final int WAGE_PER_HOUR = 20;
 
-    // Limits
-    private static final int MAX_HOURS = 100;
-    private static final int MAX_DAYS = 20;
+    public static void computeWage(String company, int wagePerHour, int maxDays, int maxHours) {
 
-    // UC7: Class Method to compute wage
-    public static void computeWage() {
+        System.out.println("\nCompany: "+company);
+
         // To track total working hours,days and accumulated wage
         int totalHours = 0;
         int totalDays = 0;
         int totalWage = 0;
 
         // Continue calculating wages until 100 hours OR 20 days limit is reached
-        while (totalHours < MAX_HOURS && totalDays < MAX_DAYS) {
+        while (totalHours < maxHours && totalDays < maxDays) {
             totalDays++;
 
             // Generate attendance: 0 = Absent, 1 = Full-time, 2 = Part-time
@@ -27,12 +20,12 @@ public class EmployeeWage {
 
             // Determines working hours based on attendance
             switch (empCheck) {
-                case IS_FULL_TIME:
+                case 1:
                     empHours = 8;
                     break;
 
-                case IS_PART_TIME:
-                    empHours = 8;
+                case 2:
+                    empHours = 4;
                     break;
 
                 default:
@@ -41,7 +34,7 @@ public class EmployeeWage {
 
             // Update total hours and total wage
             totalHours += empHours;
-            totalWage += empHours * WAGE_PER_HOUR;
+            totalWage += empHours * wagePerHour;
         }
 
         // Displaying output
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        EmployeeWage.computeWage();
+        EmployeeWage.computeWage("TCS", 20, 20, 100);
+        EmployeeWage.computeWage("Infosys", 25, 22, 120);
+        EmployeeWage.computeWage("Dmart", 18, 26, 140);
     }
 }
